9fd6e975ea82713fbcf43cf629d105a55a287f8b
sumit <sumitmessi018@gmail.com>
2023-10-11 16:22:57 +0530

    pushyourlearning

---
+++ includes.js
@@ -0,0 +1,18 @@
+//chapter 4 practice set
+let str = "har\""
+console.log(str.length)
+
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+const word = 'fox';
+console.log(sentence.includes(word))
+
+console.log(`The word "${word}" ${sentence.includes(word) ? 'is' : 'is not'} in the sentence`);
+
+
+const sentence = 'today is eleventh october second match of team india in world cup.';
+
+const word = 'india';
+console.log(sentence.includes(word))
+
+console.log(`the word "${word}" ${sentence.includes(word) ? 'is : 'is not'} in the sentence`);
